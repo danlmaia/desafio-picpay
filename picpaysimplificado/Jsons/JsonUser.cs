8c5b7adef663e55d77ff221674509364dbcdb601
Daniel da Silva Maia <daniel.maia@fitbank.com.br>
2023-09-25 17:09:32 -0300

    feat: add padronização nos campos do json de input do usuário.

--- picpaysimplificado/Jsons/JsonUser.cs
+++ picpaysimplificado/Jsons/JsonUser.cs
@@ -1,21 +1,29 @@
-﻿using picpaysimplificado.Enums;
+﻿using Newtonsoft.Json;
+using picpaysimplificado.Enums;
 
 namespace picpaysimplificado.Jsons
 {
     public class JsonUser
     {
+        [JsonProperty("firstName")]
         public string FirstName { get; set; }
 
+        [JsonProperty("lastName")]
         public string LastName { get; set; }
 
+        [JsonProperty("document")]
         public string Document { get; set; }
 
+        [JsonProperty("email")]
         public string Email { get; set; }
 
+        [JsonProperty("password")]
         public string Password { get; set; }
 
+        [JsonProperty("balance")]
         public decimal Balance { get; set; }
 
+        [JsonProperty("type")]
         public string Type { get; set; }
     }
 }
